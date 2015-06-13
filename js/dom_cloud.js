202d39af6fc9951c61d66c755ac13966f3390561
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-12 23:29:41 -1000

    added comments, cleaned up code

--- js/dom_cloud.js
+++ js/dom_cloud.js
@@ -2,13 +2,13 @@ var storage = {};
 var display = document.getElementById('dom_cloud_container');
 
 function recursiveDOM(start){
-
+  //Gathers the element's name and creates a key value pair.
   if(!storage.hasOwnProperty(start.localName)){
     storage[start.localName] = 1;
   }else{
     storage[start.localName]++;
   }
-
+  //Gathers the element's attributes and creates a value for the corresponding
   if(start.nodeType === 1){
     var attributes = start.attributes;
     for(var i = 0; i< attributes.length; i++){
@@ -19,23 +19,22 @@ function recursiveDOM(start){
       } 
     }
   }
-
   for(var i =0; i< start.children.length; i++){
       recursiveDOM(start.children[i]);
   }
-
 }
 
-recursiveDOM.convert = function(){ // converts to array;
+//Gathers data from storage and converts to multi-array
+recursiveDOM.convert = function(){ 
   var sortedArray =[]; 
   for(var key in storage){
     sortedArray.push([key,storage[key]]);
   }
-
+  //Sorts the array from highest count to lowest count
   sortedArray.sort(function(a,b){return b[1] - a[1]});
   sortedArray.splice(20,sortedArray.length - 20);
-  console.log(sortedArray);
   for(var i= 0; i<sortedArray.length; i++){
+  //Appending to display and sets the font size based on the count of each key.
   var divvy  = document.createElement('div');
   divvy.style.fontSize = (100 - i *3)/3;
   divvy.innerHTML = sortedArray[i];
@@ -43,5 +42,3 @@ recursiveDOM.convert = function(){ // converts to array;
   }
 }
 
-//kickoff
-//recursiveDOM(start);
